390d2b2f6e0cc5d8c46a2a87a3416b257de32d6d
Davide Moraschi <davidem@eurostrategy.net>
2024-01-19 09:02:15 +0100

    start spark

--- start_glue_40_vscode.cmd
+++ start_glue_40_vscode.cmd
@@ -7,6 +7,4 @@ docker run -it ^
     -e DISABLE_SSL=true ^
     --rm -p 4040:4040 ^
     -p 18080:18080 ^
-    --name aws_sdk-glue amazon/aws-glue-libs:glue_libs_4.0.0_image_01 
-
-@rem pyspark
+    --name aws_sdk-glue amazon/aws-glue-libs:glue_libs_4.0.0_image_01 pyspark
